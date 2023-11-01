1cd176f04bd2edd42364b69c58bc62f0141b15ee
nickleus27 <nickleus27@gmail.com>
2023-10-31 23:27:58 -0700

    updated comments

--- longpal.cpp
+++ longpal.cpp
@@ -31,35 +31,33 @@ public:
          * radius of odd_str[i]
          */
         std::vector<int> pal_radii(strlen, 0); // fill vector with 0's to init
-        int center = 0;                        // holds the index of the center of the largest palindrome
-        int right = 0;                         // holds the index of the
+        int center = 0;                        // holds the index of the center index of the current palindrome
+        int right = 0;                         // holds the index of the right most index of the palindrome
         int max_radius = 0;                    // track radius length of longest palindrome
         int max_center = 0;                    // track center of longest palindrome
 
         for (int i = 1; i < strlen - 1; ++i)
         {
-            // update radius at index i based on previous chars radius
+            // update radius at index i based on if i is within boundary of previous palindrome
             pal_radii[i] = (right > i) ? std::min(right - i, pal_radii[2 * center - i]) : 0;
             // expansion around i
             while (odd_str[i + 1 + pal_radii[i]] == odd_str[i - 1 - pal_radii[i]])
                 pal_radii[i]++;
-
+            // update current center of palindrome being iterated over and radius to the right
             if (i + pal_radii[i] > right)
             {
                 center = i;
                 right = i + pal_radii[i];
             }
-            // update if found palindrome is the longest
+            // keep track of longest palindrome found
             if (max_radius < pal_radii[i])
             {
-                max_radius = pal_radii[i];
-                max_center = center;
+                max_radius = pal_radii[i]; // longest palindrome radius found
+                max_center = center;       // center of longest palindrome found
             }
         }
-
-        // int max_len = *std::max_element(pal_radii.begin(), pal_radii.end());
-        // int center_index = std::distance(pal_radii.begin(), std::find(pal_radii.begin(), pal_radii.end(), max_len));
-        // return s.substr((center_index - max_len) / 2, max_len);
+        // extract substring using the center and radius.
+        // divide by two to compensate for inserted metacharacters in transformed string
         return s.substr((max_center - max_radius) / 2, max_radius);
     }
 };
